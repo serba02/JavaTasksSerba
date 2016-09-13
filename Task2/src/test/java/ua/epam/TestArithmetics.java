7f35ac23f0942da5a820046606e12bc6c76ff8fe
Lev Serba <lev_serba@epam.com>
2016-09-13 17:17:26 +0300

    add @Before

--- Task2/src/test/java/ua/epam/TestArithmetics.java
+++ Task2/src/test/java/ua/epam/TestArithmetics.java
@@ -1,6 +1,7 @@
 package ua.epam;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,13 +10,14 @@ import static org.junit.Assert.*;
  * Created by Lev_Serba on 9/13/2016.
  */
 public class TestArithmetics {
-    Arithmetics a;
+    private Arithmetics a;
+
+    @Before
     public void runT(){
         a = new Arithmetics();
     }
     @Test
     public void testAdd() throws Exception {
-        runT();
         double res = a.add(3.0, 7.0);
         Assert.assertEquals(res, 10.0, 0);
         a = null;
@@ -23,7 +25,6 @@ public class TestArithmetics {
 
     @Test
     public void testDeduct() throws Exception {
-        runT();
         double res = a.deduct(3.0, 1.0);
         Assert.assertEquals(res, 2.0, 0);
         a = null;
@@ -31,7 +32,6 @@ public class TestArithmetics {
 
     @Test
     public void testMult() throws Exception {
-        runT();
         double res = a.mult(8.0, 2.0);
         Assert.assertEquals(res, 16.0, 0);
         a = null;
@@ -39,10 +39,8 @@ public class TestArithmetics {
 
     @Test
     public void testDiv() throws Exception {
-        runT();
         double res = a.div(4.0, 2.0);
         Assert.assertEquals(res, 2.0, 0);
         a = null;
     }
-
 }
