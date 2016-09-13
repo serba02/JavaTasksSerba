aa0ada5ce5474c79b57eec57002e1e65ceb504a6
Lev Serba <lev_serba@epam.com>
2016-09-13 17:13:48 +0300

    add function runT

--- Task2/src/test/java/ua/epam/TestArithmetics.java
+++ Task2/src/test/java/ua/epam/TestArithmetics.java
@@ -9,32 +9,40 @@ import static org.junit.Assert.*;
  * Created by Lev_Serba on 9/13/2016.
  */
 public class TestArithmetics {
+    Arithmetics a;
+    public void runT(){
+        a = new Arithmetics();
+    }
     @Test
     public void testAdd() throws Exception {
-        Arithmetics a = new Arithmetics();
+        runT();
         double res = a.add(3.0, 7.0);
         Assert.assertEquals(res, 10.0, 0);
+        a = null;
     }
 
     @Test
     public void testDeduct() throws Exception {
-        Arithmetics a = new Arithmetics();
+        runT();
         double res = a.deduct(3.0, 1.0);
         Assert.assertEquals(res, 2.0, 0);
+        a = null;
     }
 
     @Test
     public void testMult() throws Exception {
-        Arithmetics a = new Arithmetics();
+        runT();
         double res = a.mult(8.0, 2.0);
         Assert.assertEquals(res, 16.0, 0);
+        a = null;
     }
 
     @Test
     public void testDiv() throws Exception {
-        Arithmetics a = new Arithmetics();
+        runT();
         double res = a.div(4.0, 2.0);
         Assert.assertEquals(res, 2.0, 0);
+        a = null;
     }
 
 }
