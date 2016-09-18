a2954811e4d5636baa8d4d341773f6d8f5bab9b9
Serba <lev_serba@epam.com>
2016-09-18 14:52:26 +0300

    All tests have been added

--- Task4/src/main/java/ua/epam/Main.java
+++ Task4/src/main/java/ua/epam/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-	    int[] array = {2,3,2,6,2,7,2,8,12,14,15,16,2,7,34,56};
+	    int[] array = {3,2,6,2,7,2,8,12,14,15,16,2,7,34,56};
         int value = 2;
         int[] arrayOfIndexes = ArrayFunctions.createArrayOfValuesIndexes(array, value);
         System.out.println(Arrays.toString(arrayOfIndexes));
--- Task4/src/test/java/ua/epam/TestArrayFunctions.java
+++ Task4/src/test/java/ua/epam/TestArrayFunctions.java
@@ -1,5 +1,6 @@
 package ua.epam;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +10,105 @@ import static org.junit.Assert.*;
  */
 public class TestArrayFunctions {
     @Test
-    public void createArrayOfValuesIndexes() throws Exception {
-
+    public void testEmptyArray() throws Exception {
+        int[] array = new int[0];
+        int value = 7;
+        int[] expectedArray = new int[0];
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+        Assert.assertArrayEquals(expectedArray, actualArray);
     }
 
+    @Test
+    public void testNoCoincidenceArray() throws Exception {
+        int[] array = {1,13,42,16,78,21};
+        int value = 17;
+        int[] expectedArray = {};
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+        Assert.assertArrayEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void testOneCoincidenceMidleArray() throws Exception {
+        int[] array = {1,13,42,16,78,21};
+        int value = 42;
+        int[] expectedArray = {2};
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+        Assert.assertArrayEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void testOneCoincidenceBeginningArray() throws Exception {
+        int[] array = {1,13,42,16,78,21};
+        int value = 1;
+        int[] expectedArray = {0};
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+        Assert.assertArrayEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void testManyCoincidenceMidleArray() throws Exception {
+        int[] array = {1,13,42,13,13,21};
+        int value = 13;
+        int[] expectedArray = {1, 3, 4};
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+        Assert.assertArrayEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void testManyCoincidenceMidleBeginningArray() throws Exception {
+        int[] array = {13,42,13,13,21};
+        int value = 13;
+        int[] expectedArray = {0, 2, 3};
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+        Assert.assertArrayEquals(expectedArray, actualArray);
+    }
+    @Test
+    public void testManyCoincidenceMidleEndArray() throws Exception {
+        int[] array = {1,13,13,12,13};
+        int value = 13;
+        int[] expectedArray = {1, 2, 4};
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+        Assert.assertArrayEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void testManyCoincidenceFullArray() throws Exception {
+        int[] array = {13,13,13,13,13};
+        int value = 13;
+        int[] expectedArray = {0,1,2,3,4};
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+        Assert.assertArrayEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void testMaxIntValueInArray() throws Exception {
+        int[] array = {12,Integer.MAX_VALUE,42,13,7};
+        int value = Integer.MAX_VALUE;
+        int[] expectedArray = {1};
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+        Assert.assertArrayEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void testMinIntValueInArray() throws Exception {
+        int[] array = {12,Integer.MIN_VALUE,42,13,7};
+        int value = Integer.MIN_VALUE;
+        int[] expectedArray = {1};
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+        Assert.assertArrayEquals(expectedArray, actualArray);
+    }
+
+    @Test(expected = NegativeArraySizeException.class)
+    public void testNegativeArraySizeException() throws Exception {
+        int[] array = new int[-2];
+        int value = 4;
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullPointer() throws Exception {
+        int[] array = null;
+        int value = 21;
+        int[] actualArray = ArrayFunctions.createArrayOfValuesIndexes(array, value);
+    }
 }
